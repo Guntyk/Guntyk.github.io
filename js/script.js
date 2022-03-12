5374cb94793703bd82accb198666a95143292518
Guntyk <87826661+Guntyk@users.noreply.github.com>
2022-03-12 21:25:46 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ $('ul.selector__menu').on('click', 'li:not(.selector__item_active)', function() 
 		.closest('body').find('div.block__content').removeClass('block__content_active').eq($(this).index()).addClass('block__content_active');
 });
 VanillaTilt.init(document.querySelectorAll(".dancer__item"), {
-    max: 15,
+    max: 10,
     speed: 1000,
 });
 VanillaTilt.init(document.querySelectorAll(".dancer__item_last"), {
-    max: 10,
+    max: 5,
     speed: 1000,
 });
